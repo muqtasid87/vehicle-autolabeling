7187281660a1228049229ae56cd73a9557e7bb8d
muqtasid87 <muqtasid87@gmail.com>
2025-09-02 13:44:15 +0800

    reduced batch size to 1 for finetuning

--- run_finetune.py
+++ run_finetune.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     custom_hyperparams = {
         "num_train_epochs": 1,
         "learning_rate": 5e-5,
-        "per_device_train_batch_size": 2,
+        "per_device_train_batch_size": 1,
     }
 
     run_finetuning(
